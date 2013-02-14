df961baea02a05c791901de9d4c52eff23f9eec1
Chris Simon <info@decomputeur.nl>
2013-02-14 19:35:52 +0100

    Initial commit

---
+++ include/config.php
@@ -0,0 +1,6 @@
+<?php
+define("HOST", "localhost"); // The host you want to connect to.
+define("USER", "comput6"); // The database username.
+define("PASSWORD", "Eag!e00789"); // The database password. 
+define("DATABASE", "activity"); // The database name.
+?>
---
+++ include/db_connect.php
@@ -0,0 +1,5 @@
+<?php
+include 'config.php';
+
+$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
+?>
